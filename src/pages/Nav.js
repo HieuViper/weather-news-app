0009c038982b35063afb80fcd9d0989e1c8f7498
HieuViper <mr.hieu2491@gmail.com>
2021-12-17 14:13:36 +0700

    add active navbar

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom';
 import './Nav.css'
 function NavBar() {
     return (
-        <ul>
-            <li><Link class="active" to="/">Home</Link></li>
-            <li><Link to="/news">News</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/about">About</Link></li>
+        <ul id="container">
+            <li><Link class="item active" to="/">Home</Link></li>
+            <li><Link class="item" to="/news">News</Link></li>
+            <li><Link class="item" to="/contact">Contact</Link></li>
+            <li><Link class="item" to="/about">About</Link></li>
         </ul>
     )
+}
+
+//active navbar
+let items = document.getElementsByClassName("item");
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
 }
 export default NavBar
